2e55077680546670cb2031329f3993d4aa794fb7
UF-Elektron <dave.mueller@gmx.net>
2024-09-19 08:51:29 +0000

    device examples for device registry changed + comments

--- homeassistant/components/wiser_by_feller/device.py
+++ homeassistant/components/wiser_by_feller/device.py
@@ -10,6 +10,8 @@ from homeassistant.const import (
     ATTR_MANUFACTURER,
     ATTR_MODEL,
     ATTR_NAME,
+    # use rooms id from load object for suggested area (room name is in rooms object)
+    # ATTR_SUGGESTED_AREA,
     ATTR_SW_VERSION,
     ATTR_VIA_DEVICE,
 )
@@ -60,6 +62,7 @@ class Device:
     metadata: DeviceMetaData
 
 
+# TODO: wie mappen wir am besten die source addresse, den load channel mit der load-id?
 async def async_setup_devices(bridge):
     """Manage setup of devices from Hue devices."""
     entry = bridge.config_entry
@@ -68,15 +71,25 @@ async def async_setup_devices(bridge):
     dev_reg = dr.async_get(hass)
     # use test value
     # dev_controller = api.devices
-    tmp = DeviceProductData("1", "2", "3", "4")
-    tmp_meta = DeviceMetaData("jajajaaaa")
-    a_device = Device("42", tmp, tmp_meta)
-    a_device.product_data.product_name = "test_name"
-    a_device.product_data.model_id = "69"
-    a_device.product_data.software_version = "1234"
-    a_device.product_data.manufacturer_name = "chinesli"
-    a_device.metadata.name = "noname"
-    dev_controller = [a_device]
+    tmp_prod_data_1 = DeviceProductData("1", "2", "3", "4")
+    tmp_meta_ugw = DeviceMetaData("ugw")
+    a_ugw = Device("42", tmp_prod_data_1, tmp_meta_ugw)
+    a_ugw.product_data.product_name = "test_name"
+    a_ugw.product_data.model_id = "69"
+    a_ugw.product_data.software_version = "1234"
+    a_ugw.product_data.manufacturer_name = "chinesli"
+    # a_ugw.metadata.name = "noname"
+
+    tmp_prod_data_2 = DeviceProductData("9", "9", "9", "9")
+    tmp_meta_device = DeviceMetaData("device")
+    a_device = Device("5555", tmp_prod_data_2, tmp_meta_device)
+    a_device.product_data.product_name = "device_blabla"
+    a_device.product_data.model_id = "694288"
+    a_device.product_data.software_version = "1234.5678"
+    a_device.product_data.manufacturer_name = "-"
+    # a_device.metadata.name = "noname"
+
+    dev_controller = [a_ugw, a_device]
     # dev_controller = [
     #     {
     #         "product_data": {
@@ -103,12 +116,14 @@ async def async_setup_devices(bridge):
             ATTR_MANUFACTURER: hue_resource.product_data.manufacturer_name,
         }
         # DME: is it a normal device or the uGateway? copy from hue devices.py
-        # for now handle it like it's always a bridge/uGW
         # if hue_resource.metadata.archetype == DeviceArchetypes.BRIDGE_V2:
-        #     params[ATTR_IDENTIFIERS].add((DOMAIN, api.config.bridge_id))
-        # else:
-        #     params[ATTR_VIA_DEVICE] = (DOMAIN, api.config.bridge_device.id)
-        params[ATTR_IDENTIFIERS].add((DOMAIN, api.bridge_id))
+        if hue_resource.metadata.name == "ugw":
+            print("attribute identified as ugw")
+            params[ATTR_IDENTIFIERS].add((DOMAIN, api.bridge_id))
+        else:
+            print("attribute identified as device")
+            # params[ATTR_VIA_DEVICE] = (DOMAIN, api.config.bridge_device.id)
+            params[ATTR_VIA_DEVICE] = (DOMAIN, "3434343434343")  # id of uGW
         return dev_reg.async_get_or_create(config_entry_id=entry.entry_id, **params)
 
     known_devices = [add_device(hue_device) for hue_device in dev_controller]
